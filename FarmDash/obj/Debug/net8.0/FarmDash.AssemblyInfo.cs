ea09b0eb062749b9fcec26d24926de76939a54cf
Harrison Voss <hvoss@iastate.edu>
2025-08-14 11:58:15 -0500

    made the home screen a bit better with cards to other pages

--- FarmDash/obj/Debug/net8.0/FarmDash.AssemblyInfo.cs
+++ FarmDash/obj/Debug/net8.0/FarmDash.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FarmDash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+714dfd3441f0bc6b13f205ba92724d7773521661")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba0fceda1a2fe026aea28d01ca39bae49781b006")]
 [assembly: System.Reflection.AssemblyProductAttribute("FarmDash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FarmDash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
